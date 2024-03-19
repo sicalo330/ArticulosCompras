83c31e8d01042c49189eb6b397d21337c4ea4de4
sicalo330 <noah.esteban9@gmail.com>
2024-03-19 10:57:42 -0500

    Hacer conexión con celular

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    endpoint: 'http://localhost:4040',
+    endpoint: 'http://192.168.254.60:4040',
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
     production: false,
-    endpoint: 'http://localhost:4040',
+    endpoint: 'http://192.168.254.60:4040',
   };
+
+  //IP del wifi para conectar el celular 192.168.254.60:4200
+  //Comando para compilar ng serve --host 192.168.254.60 --disable-host-check 
